356ea04c4f836f41bd54b5390c1debffa515651f
CDamiang <128390008+CDamiang@users.noreply.github.com>
2024-11-13 03:33:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ButtonGoogle/ButtonGoogle.tsx
@@ -0,0 +1,29 @@
+
+"use client";
+import React from "react";
+import { FaGoogle } from "react-icons/fa6";
+import Button from "../Button/Button";
+import { signIn, useSession } from "next-auth/react";
+
+const GoogleLoginButton = () => {
+  const { data: session } = useSession();
+  console.log(session);
+
+  return (
+    <Button
+      onClick={async () => {
+        await signIn("google", {
+          callbackUrl: "/",
+        });
+      }}
+      className="flex flex-row justify-center items-center w-[225px] "
+    >
+      <span className="me-2">
+        <FaGoogle />
+      </span>
+      <span className="text-center">Continuar con Google</span>
+    </Button>
+  );
+};
+
+export default GoogleLoginButton;
